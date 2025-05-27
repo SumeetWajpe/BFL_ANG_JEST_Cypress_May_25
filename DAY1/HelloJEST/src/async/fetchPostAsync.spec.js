d6acbb138e0b5820e8cf207e0b3db4cb08b64190
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-05-27 14:54:38 +0530

    ✨ testing async code with promises

--- DAY1/HelloJEST/src/async/fetchPostAsync.spec.js
+++ DAY1/HelloJEST/src/async/fetchPostAsync.spec.js
@@ -1,24 +1,38 @@
 const postModule = require("./fetchPostAsync");
-describe("test suite for async fetchPostAsync", () => {
-  it("tests for fetchPostAsync", async () => {
-    try {
-      // arrange
+describe("test suite for async fetchPost", () => {
+  //   it("tests for fetchPost using async/await", async () => {
+  //     try {
+  //       // arrange
+  //       var mockResult = {
+  //         userId: 1,
+  //         id: 1,
+  //         title:
+  //           "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  //         body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  //       };
+  //       // act
+  //       const post = await postModule.fetchPost(1);
 
-      var mockResult = {
-        userId: 1,
-        id: 1,
-        title:
-          "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-      };
-      // act
-
-      const post = await postModule.fetchPost(1);
-
+  //       // assert
+  //       expect(post).toEqual(mockResult);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   });
+  it("tests for fetchPost using promise", () => {
+    // arrange
+    var mockResult = {
+      userId: 1,
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    };
+    // act
+    const thePromise = postModule.fetchPost(1);
+    thePromise.then(post => {
       // assert
       expect(post).toEqual(mockResult);
-    } catch (error) {
-      console.log(error);
-    }
+    });
   });
 });
