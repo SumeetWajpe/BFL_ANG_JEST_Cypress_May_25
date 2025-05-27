a1bc22a4322b8c193eecdad8e759f85a1e134299
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-05-27 10:39:48 +0530

    ✨ Test Suite with describe() & test case with it()

--- DAY1/HelloJEST/src/add.spec.js
+++ DAY1/HelloJEST/src/add.spec.js
@@ -1,10 +1,30 @@
 const Add = require("./add");
-test("Test case for addition", function () {
-  // Arrange
-  let result = null;
-  // Act
-  result = Add(20, 30);
+// JEST test method
+// test("Test case for addition", function () {
+//   // Arrange
+//   let result = null;
+//   // Act
+//   result = Add(20, 30);
 
-  // Assert
-  expect(result).toBe(50);
+//   // Assert
+//   expect(result).toBe(50);
+// });
+
+describe("Test suite for addition test cases", () => {
+  it("tests addition of two numbers", () => {
+    // Arrange
+    let result = null;
+    // Act
+    result = Add(20, 30);
+    // Assert
+    expect(result).toBe(60);
+  });
+  it("tests addition of two strings", () => {
+    // Arrange
+    let result = null;
+    // Act
+    result = Add("Hello", "JEST");
+    // Assert
+    expect(result).toBe("HelloJEST");
+  });
 });
