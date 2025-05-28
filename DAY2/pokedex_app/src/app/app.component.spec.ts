5f51669f15efa36f331919bb0cfc5fb2f6a8d926
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-05-28 12:03:11 +0530

    ✨ Testing for HTML using nativeElement (querySelector)

--- DAY2/pokedex_app/src/app/app.component.spec.ts
+++ DAY2/pokedex_app/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
 import { LoaderService } from "./shared/services/loader.service";
+import { DebugElement } from "@angular/core";
 describe("test suite for App Component", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,5 +25,14 @@ describe("test suite for App Component", () => {
     const fixture = TestBed.createComponent(AppComponent); // instantiation of AppComponent
     const app = fixture.componentInstance; // fixture - metadata along with component instance info
     expect(app.title).toEqual("pokedex-angular");
+  });
+
+  fit("tests if h1 tag is having header class generated", () => {
+    // nativeElement returns a DOM tree
+    // debugElement -> is Angular Object and has additional methods of reference
+    const fixture = TestBed.createComponent(AppComponent); // instantiation of AppComponent
+    const el: DebugElement = fixture.debugElement;
+    const h1 = el.nativeElement.querySelector("h1.header");
+    expect(h1).toBeTruthy();
   });
 });
