b24e1ea6750514c99b4b3c7311d9aa0a8d4202ea
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-05-28 14:39:32 +0530

    ✨ testing for service Instace (Dep Injected service instance) in Pokemon List Component

---
+++ DAY2/pokedex_app/src/app/components/pokemon-list/pokemon-list.component.spec.ts
@@ -0,0 +1,49 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { PokemonListComponent } from "./pokemon-list.component";
+import { PokemonItemComponent } from "../pokemon-item/pokemon-item.component";
+import { FilterPokemonPipe } from "../../shared/pipes/filter-pokemon.pipe";
+import { HeaderComponent } from "../../shared/components/header/header.component";
+import { StatsFilterComponent } from "../../shared/components/stats-filter/stats-filter.component";
+import { DialogComponent } from "../../shared/components/dialog/dialog.component";
+import { PokemonDetailsComponent } from "../../components/pokemon-details/pokemon-details.component";
+import { PokemonService } from "../../shared/services/pokemon.service";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { FormsModule } from "@angular/forms";
+import { NgMultiSelectDropDownModule } from "ng-multiselect-dropdown";
+
+describe("test suites for Pokemon List Component", () => {
+  let httpMock: HttpTestingController;
+  let componentInstance: PokemonListComponent;
+  let fixture: ComponentFixture<PokemonListComponent>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        PokemonListComponent,
+        PokemonItemComponent,
+        FilterPokemonPipe,
+        HeaderComponent,
+        StatsFilterComponent,
+        DialogComponent,
+        PokemonDetailsComponent,
+      ],
+      providers: [PokemonService],
+      imports: [
+        HttpClientTestingModule,
+        FormsModule,
+        NgMultiSelectDropDownModule,
+      ],
+    }).compileComponents();
+
+    httpMock = TestBed.inject(HttpTestingController); // injects HttpClient & its a mock backend for testing HttpRequests
+    fixture = TestBed.createComponent(PokemonListComponent);
+    componentInstance = fixture.componentInstance;
+  });
+
+  it("tests for the service instance to be created", () => {
+    const servInstance = fixture.debugElement.injector.get(PokemonService);
+    expect(servInstance).toBeTruthy();
+  });
+});
