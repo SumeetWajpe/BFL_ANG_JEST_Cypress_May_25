e8922e63136ac9dd053edfb1b2439c168172760e
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-05-28 11:39:08 +0530

    ✨ using beforeEach() to consist of resuable code for the two tests

--- DAY2/pokedex_app/src/app/app.component.spec.ts
+++ DAY2/pokedex_app/src/app/app.component.spec.ts
@@ -3,19 +3,26 @@ import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
 import { LoaderService } from "./shared/services/loader.service";
 describe("test suite for App Component", () => {
-  it("should create App Component instance", async () => {
-    // arrange
+  beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
       providers: [LoaderService],
       imports: [RouterTestingModule],
     }).compileComponents(); // compileComponents() - compile the css and html
-
+  });
+  it("should create App Component instance", () => {
     // act
     const fixture = TestBed.createComponent(AppComponent); // instantiation of AppComponent
     const app = fixture.componentInstance; // fixture - metadata along with component instance info
 
     // assert
     expect(app).toBeTruthy();
+  });
+
+  it("should test if title is equal to pokedex-angular", () => {
+    // act
+    const fixture = TestBed.createComponent(AppComponent); // instantiation of AppComponent
+    const app = fixture.componentInstance; // fixture - metadata along with component instance info
+    expect(app.title).toEqual("pokedex-angular");
   });
 });
