5d6c77656341fbd962120c6fe3845d54e334d876
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-05-29 14:53:55 +0530

    ✨ using fixtures (basics)

---
+++ DAY3/FirstCypressApp/cypress/e2e/lambdatest.cy.js
@@ -0,0 +1,23 @@
+describe("test suite for lambdatest.io ", () => {
+  //   it("should login successfully", () => {
+  //     cy.visit(
+  //       "https://ecommerce-playground.lambdatest.io/index.php?route=account/login",
+  //     );
+  //     cy.get("#input-email").type("aniketadmin@gmail.com");
+  //     cy.get("#input-password").type("admin@1");
+  //     cy.get("form > .btn").click();
+  //     cy.url().should(
+  //       "eq",
+  //       "https://ecommerce-playground.lambdatest.io/index.php?route=account/account",
+  //     );
+  //   });
+  it("should login successfully but using fixtures", () => {
+    cy.visit(
+      "https://ecommerce-playground.lambdatest.io/index.php?route=account/login",
+    );
+
+    cy.fixture("login").then(data => console.log(data));
+
+   
+  });
+});
