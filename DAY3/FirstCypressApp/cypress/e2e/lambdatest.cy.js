c4aba5d7065ed65fbd92b82323d2f6ceb0ec0730
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-05-29 15:12:49 +0530

    ✨ using before to initialize global loginData object

--- DAY3/FirstCypressApp/cypress/e2e/lambdatest.cy.js
+++ DAY3/FirstCypressApp/cypress/e2e/lambdatest.cy.js
@@ -1,4 +1,10 @@
-describe("test suite for lambdatest.io ", () => {
+describe("test suite for lambdatest.io ", function () {
+  before(function () {
+    cy.fixture("login").then(function (data) {
+      this.loginData = data;
+    });
+  });
+
   //   it("should login successfully", () => {
   //     cy.visit(
   //       "https://ecommerce-playground.lambdatest.io/index.php?route=account/login",
@@ -11,19 +17,34 @@ describe("test suite for lambdatest.io ", () => {
   //       "https://ecommerce-playground.lambdatest.io/index.php?route=account/account",
   //     );
   //   });
-  it("should login successfully but using fixtures", () => {
+  //   it("should login successfully but using fixtures", () => {
+  //     cy.visit(
+  //       "https://ecommerce-playground.lambdatest.io/index.php?route=account/login",
+  //     );
+
+  //     cy.fixture("login").then(data => {
+  //       cy.get("#input-email").type(data.email);
+  //       cy.get("#input-password").type(data.pwd);
+  //       cy.get("form > .btn").click();
+  //       cy.url().should(
+  //         "eq",
+  //         "https://ecommerce-playground.lambdatest.io/index.php?route=account/account",
+  //       );
+  //     });
+  //   });
+  it("should login successfully but using fixtures", function () {
     cy.visit(
       "https://ecommerce-playground.lambdatest.io/index.php?route=account/login",
     );
 
-    cy.fixture("login").then(data => {
-      cy.get("#input-email").type(data.email);
-      cy.get("#input-password").type(data.pwd);
-      cy.get("form > .btn").click();
-      cy.url().should(
-        "eq",
-        "https://ecommerce-playground.lambdatest.io/index.php?route=account/account",
-      );
-    });
+    // console.log(this.loginData);
+    //fetching data from fixtures fetch in before -> beforeAll
+    cy.get("#input-email").type(this.loginData.email);
+    cy.get("#input-password").type(this.loginData.pwd);
+    cy.get("form > .btn").click();
+    cy.url().should(
+      "eq",
+      "https://ecommerce-playground.lambdatest.io/index.php?route=account/account",
+    );
   });
 });
