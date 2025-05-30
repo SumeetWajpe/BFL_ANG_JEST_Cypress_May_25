51e8063d64fc15bf8eb710f2da05d881f0e721f0
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-05-30 10:58:33 +0530

    ✨ Component Testing - Cypress -passing the componentProperties (model)

--- DAY4/ang_cypress_component_testing_1/cypress/AppComponent.cy.ts
+++ DAY4/ang_cypress_component_testing_1/cypress/AppComponent.cy.ts
@@ -1,8 +1,27 @@
 import { AppComponent } from '../src/app/app.component';
 
 describe('test suite for AppComponent.cy.ts', () => {
+  // it('should instantiate (mount) App Component', () => {
+  //   // 1st way
+  //   cy.mount(AppComponent); // compile anf render it in the browser
+  // });
+  // it('should instantiate (mount) App Component', () => {
+  //   // 2nd way - mocking dependencies
+  //   cy.mount(AppComponent, {
+  //     declarations: [AppComponent],
+  //     providers: [],
+  //     imports: [],
+  //   }); // compile and render it in the browser
+  //   cy.get('h1').should('contain', 'Hello, ang_cypress_component_testing_1');
+  // });
+
   it('should instantiate (mount) App Component', () => {
-    // 1st way
-    cy.mount(AppComponent); // compile anf render it in the browser
+    // 3nd way - passing the model data
+    cy.mount(AppComponent, {
+      componentProperties: {
+        title: 'Component testing using Cypress',
+      },
+    }); // compile and render it in the browser
+    cy.get('h1').should('contain', 'Component testing using Cypress');
   });
 });
